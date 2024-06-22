1951883e5e9fa0f18baf70d877f21b26a2094c57
silviaphungky <silvia.phungky@mandirisek.co.id>
2024-06-23 00:47:43 +0700

    netlify config

---
+++ src/api.js
@@ -0,0 +1,49 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+const serverles = require('serverless-http')
+
+// import
+require('../db/connection')
+const Animation = require('../models/Animation')
+
+app.all('/*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+
+  next()
+})
+const router = express.Router()
+
+// routes
+router.get('/', (req, res) => {
+  res.json({
+    data: 'success',
+  })
+})
+
+router.post('/animation', (req, res) => {
+  try {
+    const { json } = req.body
+    const animation = new Animation({ json })
+    animation.save()
+
+    return res.status(201).json({ message: 'Successfully create animation' })
+  } catch (error) {
+    console.log(error, 'Failed create animation')
+  }
+})
+
+app.use('/.netlify/functions/api', router)
+
+// const port = process.env.PORT || 8000
+
+// app.listen(port, () => {
+//   console.log('listening on port', port)
+// })
+
+module.exports.handler = serverles(app)
