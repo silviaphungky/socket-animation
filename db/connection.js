24324b8b87910d3d68accff9ba000a5727afb5ad
silviaphungky <silvia.phungky@mandirisek.co.id>
2024-06-23 09:57:09 +0700

    test get direct url

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
-const uri = process.env.DB_URL
-
+const uri =
+  'mongodb+srv://silviaphungky7:Silviasf16@cluster0.ruvtcc7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 mongoose
   .connect(uri, {})
   .then(() => console.log('connected to db'))
