dd49e393efcf88d6e615ea246ad2afb2f2f9962f
silviaphungky <silvia.phungky@mandirisek.co.id>
2024-06-23 13:20:07 +0700

    socket server

--- db/connection.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-const uri = process.env.DB_URL
-mongoose
-  .connect(uri, {})
-  .then(() => console.log('connected to db'))
-  .catch((error) => console.log('error connect to db', error))
--- src/api.js
+++ src/api.js
@@ -3,55 +3,13 @@ require('dotenv').config()
 const io = require('socket.io')(8080)
 
 io.on('connection', (socket) => {
-  console.log('socket connected **********')
-  // socket.on('sendTest', (params) => {
-  //   socket.params = params
-  // })
-  // io.emit('getTest', socket.params)
+  console.log('connected')
+  socket.on('sendTest', (params) => {
+    socket.params = params
+  })
+  socket.emit('getTest', socket.params)
 })
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-const serverless = require('serverless-http')
-
-// import
-require('../db/connection')
-const Animation = require('../models/Animation')
-
-app.all('/*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
-
-  next()
-})
-const router = express.Router()
-
-// routes
-router.get('/', (req, res) => {
-  res.json({
-    data: 'success',
-  })
-})
-
-router.post('/animation', (req, res) => {
-  try {
-    const { json } = req.body
-    Animation.create(req.body).then((animation) =>
-      res
-        .status(201)
-        .json({ message: 'Successfully create animation', data: animation })
-    )
-  } catch (error) {
-    console.log(error, 'Failed create animation')
-  }
-})
-
-app.use('/.netlify/functions/api', router)
-
-const handler = serverless(app)
-module.exports.handler = async (event, context) => {
-  const result = await handler(event, context)
-  return result
-}
